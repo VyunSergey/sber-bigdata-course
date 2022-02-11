002960a95d485a346898a7ccc4d16240550fc147
Vyun Sergey <vyun.sergey@icloud.com>
2022-02-11 15:09:53 +0300

    add UDFs

---
+++ src/main/scala/ru/sberbank/bigdata/study/course/sales/spark/SparkUDFs.scala
@@ -0,0 +1,16 @@
+package ru.sberbank.bigdata.study.course.sales.spark
+
+import org.apache.spark.sql.expressions.UserDefinedFunction
+import org.apache.spark.sql.functions.udf
+import org.apache.spark.sql.types.BooleanType
+
+object SparkUDFs extends Serializable {
+  val coordinateNeighbours: (java.math.BigDecimal, java.math.BigDecimal, java.math.BigDecimal, java.math.BigDecimal) => Boolean =
+    (latX, lonX, latY, lonY) => {
+      latX.add(latY.negate).abs.compareTo(new java.math.BigDecimal(0.01)) <= 0.0 &&
+        lonX.add(lonY.negate).abs.compareTo(new java.math.BigDecimal(0.01)) <= 0.0 &&
+        latX.add(latY.negate).abs.add(lonX.add(lonY.negate).abs).compareTo(new java.math.BigDecimal(0.0)) > 0.0
+    }
+
+  val neighboursUDF: UserDefinedFunction = udf(coordinateNeighbours, BooleanType)
+}
